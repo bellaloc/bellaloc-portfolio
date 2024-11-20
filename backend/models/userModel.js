77f52b0a566bc2e6598cc64d8fb5f5a7b859ff38
Loc <loc@locs-air.mynetworksettings.com>
2024-11-20 08:33:36 -0800

    backend/models/usermodel.js

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'Please use a valid email address']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
