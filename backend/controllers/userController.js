21b744ea00f90802dec3d0a2ec1b52ff02435e81
Loc <loc@locs-air.mynetworksettings.com>
2024-11-20 08:31:56 -0800

    backend/controllers/usercontroller.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+const userService = require('../services/userService');
+
+// Create a new user
+exports.createUser = async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Get all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
